f5a7b7ffa22a5bd4d1348450b38dd57f413a19e4
Fernando Jesus Hernandez <64885403+fjh321@users.noreply.github.com>
2023-11-11 12:28:38 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,5 +4,3 @@ module.exports = {
     'url': 'mongodb+srv://fernandojhernandez123:4LNTFk81dJ1Oeup9@cluster0.vyoxyvg.mongodb.net/test?retryWrites=true&w=majority',
     'dbName': 'demo'
 };
-
-// 4LNTFk81dJ1Oeup9
